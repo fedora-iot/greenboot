f3120a88b4b90ac5b497a396b9aafab40b67fc52
Christian Glombek <lorbus@fedoraproject.org>
2018-10-08 15:37:26 +0200

    Add more comments to 01_ostree_grub2_fallback.sh script

--- etc/greenboot/green.d/01_ostree_grub2_fallback.sh
+++ etc/greenboot/green.d/01_ostree_grub2_fallback.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-# We need to determine whether this boot was a fallback boot
+# Determine if the current boot is a fallback boot
 if grub2-editenv list | grep -q "^boot_counter=-1$"; then
+  # If booted into fallback deployment, clean up bootloader entries (rollback)
   rpm-ostree rollback
 fi
