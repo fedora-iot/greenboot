e09797215729ea6bf0f05ba8a8896ca8044f4795
Christian Glombek <cglombek@redhat.com>
2019-04-01 13:59:38 +0200

    Make fallback check script a systemd service

--- etc/greenboot/green.d/01_ostree_grub2_fallback.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Determine if the current boot is a fallback boot
-if grub2-editenv list | grep -q "^boot_counter=-1$"; then
-  # If booted into fallback deployment, clean up bootloader entries (rollback)
-  rpm-ostree rollback
-fi
