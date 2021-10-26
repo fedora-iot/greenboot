25417b6a36583a32fc6d79566d2ae2c6fbc64aa9
Jose Noguera <jnoguera@redhat.com>
2021-10-26 15:04:02 +0100

    Wip

--- etc/greenboot/check/wanted.d/02_check_services.sh
+++ etc/greenboot/check/wanted.d/02_check_services.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-SERVICES=( sshd NetworkManager greenboot-healthcheck 1sshd )
+check_if_config_file_exists() {
+    GREENBOOT_CONFIGURATION_FILE=/etc/greenboot/greenboot.conf
+    if test -f "$GREENBOOT_CONFIGURATION_FILE"; then
+        source $GREENBOOT_CONFIGURATION_FILE
+        # SERVICES=( "${SERVICES[@]}" "${GREENBOOT_MONITOR_SERVICES[@]}" )
+        SERVICES=( "${GREENBOOT_MONITOR_SERVICES[@]}" )
+    else
+        echo "ERROR: $GREENBOOT_CONFIGURATION_FILE doesn't exist"
+        exit 2
+    fi
+}
 
 check_if_service_exists() {
-    systemctl list-units | grep --quiet "$service".service || rc=$?
-    if [[ -n $rc ]]; then
-        echo "$service doesn't exist"
-        return 1
-    fi
-    return 0
+    local service=$1
+    (systemctl list-units | grep --quiet "$service".service)
+    echo "$?"
 }
 
 is_service_active() {
-    systemctl is-active --quiet "$service".service || rc=$?
+    local service=$1
+    systemctl is-active --quiet "$service".service || local rc=$?
     if [[ -n $rc ]]; then
         echo "$service is down, rebooting"
         exit 1
     fi
 }
 
-for service in ${SERVICES[*]}; do
+
+check_if_config_file_exists
+for service in "${SERVICES[@]}"; do
     service_exists=$(check_if_service_exists "$service")
-    if [[ service_exists -eq 0 ]]; then
+    if [[ $service_exists -ne 0 ]]; then
+        echo "WARNING: $service.service can't be found"
+    else
         is_service_active "$service"
     fi
 done
